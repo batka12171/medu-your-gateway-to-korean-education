0df13ba3fea242ddeed0ca249074e38818d2bab1
Batmunkh Boldbaatar <boldbaatarbatmunkh1217@gmail.com>
2026-03-19 20:04:44 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import UniversityCarousel from "../components/home/UniversityCarousel";
 import UniversitiesPreview from "../components/home/UniversitiesPreview";
 import MentorsSection from "../components/home/MentorsSection";
 import CommunityPreview from "../components/home/CommunityPreview";
+import UniversityTicker from "../components/home/UniversityTicker";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <UniversityTicker />
       <GetAdmittedSection />
       <UniversityCarousel />
       <StatsSection />
