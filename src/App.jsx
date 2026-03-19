561b521ddd04a0c9db169a1379a366bc162b68e7
Batmunkh Boldbaatar <boldbaatarbatmunkh1217@gmail.com>
2026-03-19 20:16:45 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,11 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/AdmissionRoadmap" element={
+        <LayoutWrapper currentPageName="AdmissionRoadmap">
+          <AdmissionRoadmap />
+        </LayoutWrapper>
+      } />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
